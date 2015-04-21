1da28863c0709b7a6d62c273909820ce3bdaf864
wangshuai <wangshuai@itom-ws.sc.mdcl.com.cn>
2015-04-21 13:09:43 +0800

    hibernate官方文档示例测试

---
+++ src/main/java/org/hibernate/tutorial/util/HibernateUtil.java
@@ -0,0 +1,40 @@
+package org.hibernate.tutorial.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+			/*	
+            return new Configuration().configure().buildSessionFactory(
+			    new StandardServiceRegistryBuilder().build() );
+			*/
+        	/**/
+			Configuration cfg = new Configuration().configure();
+			System.out.println(cfg.getProperties());
+			StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(cfg.getProperties()).build();
+			SessionFactory sf = cfg.buildSessionFactory(serviceRegistry);
+			Session session = sf.openSession();
+			return sf;
+			
+        }
+        catch (Throwable ex) {
+            // Make sure you log the exception, as it might be swallowed
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+}
